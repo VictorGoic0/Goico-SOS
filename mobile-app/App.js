4593988d0398ac0bc6b405f0acaafce251da1bcd
Victor Goico <vgoico000@citymail.cuny.edu>
2025-10-27 00:02:20 -0400

    Supress warning

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,8 +1,15 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect } from "react";
+import { LogBox } from "react-native";
 import * as Notifications from "expo-notifications";
 import "./src/config/firebase"; // Initialize Firebase
 import AppNavigator, { navigationRef } from "./src/navigation/AppNavigator";
+
+// // Suppress known warnings that don't affect functionality
+// LogBox.ignoreLogs([
+//   "expo-notifications",
+//   "`expo-notifications` functionality is not fully supported in Expo Go",
+// ]);
 
 export default function App() {
   // Set up notification response listener (when user taps notification)
